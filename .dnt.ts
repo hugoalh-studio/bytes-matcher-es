10e43e055c4915c9cf2221666e1221be3cb81577
hugoalh <hugoalh@gmail.com>
2024-10-19 17:02:19 +0800

    Update resources

--- .dnt.ts
+++ .dnt.ts
@@ -8,11 +8,11 @@ await invokeDenoNodeJSTransformer({
 		"LICENSE.md",
 		"README.md"
 	],
-	entrypoints: configJSR.exports,
+	entrypoints: configJSR.getExports(),
 	generateDeclarationMap: true,
 	metadata: {
-		name: "@hugoalh/bytes-matcher",
-		version: configJSR.version,
+		name: configJSR.getName(),
+		version: configJSR.getVersion(),
 		description: "A module to determine whether the bytes is match the specify signature.",
 		keywords: [
 			"bytes",
